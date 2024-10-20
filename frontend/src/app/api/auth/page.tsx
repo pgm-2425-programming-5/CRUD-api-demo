784cde735aafb8f95e05310f76fe9be6208eeec1
jacques <janneslambrecht@hotmail.com>
2024-10-20 17:11:05 +0200

    fix import

--- frontend/src/app/api/auth/page.tsx
+++ frontend/src/app/api/auth/page.tsx
@@ -2,11 +2,8 @@ interface User {
   name: string;
 }
 
-interface Props {
-  user: User;
-}
 
-function HelloMessage({ user }: Props) {
+function HelloMessage(user : User) {
   return (
     <div>
       <h1>Hello, {user.name}!</h1>
