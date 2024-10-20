4b76eb8c6df33e9b0bc3157217fdd1a7df348615
jacques <janneslambrecht@hotmail.com>
2024-10-20 14:54:07 +0200

    update

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,9 +1,18 @@
 export default () => ({
-    "users-permissions": {
+  "users-permissions": {
     config: {
       register: {
         allowedFields: ["nickname"],
       },
     },
+  },
+  "graphql": {
+    enabled: true,
+    config: {
+      playgroundAlways: true,
+    },
+  },
+  "apollo-sandbox": {
+    enabled: true,
   },
 });
