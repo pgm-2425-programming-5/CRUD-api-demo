50cab184b10903f2363ab3153436d25920c41f44
Jacques <janneslambrecht@hotmail.com>
2024-10-21 13:02:31 +0200

    initial

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,19 +1,3 @@
-import type { Struct, Schema } from '@strapi/strapi';
-
-export interface NavigationTest extends Struct.ComponentSchema {
-  collectionName: 'components_navigation_tests';
-  info: {
-    displayName: 'test';
-  };
-  attributes: {
-    teest: Schema.Attribute.String;
-  };
-}
-
-declare module '@strapi/strapi' {
-  export module Public {
-    export interface ComponentSchemas {
-      'navigation.test': NavigationTest;
-    }
-  }
-}
+/*
+ * The app doesn't have any components yet.
+ */
